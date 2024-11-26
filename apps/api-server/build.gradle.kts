ba6c92b762c43d64cf508f8400d5710452274268
evergreen <tkdfhr7497@naver.com>
2024-11-27 00:53:40 +0900

    기본 애플리케이션 생성

---
+++ apps/api-server/build.gradle.kts
@@ -0,0 +1,12 @@
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+}
